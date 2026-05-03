e7e3c02fc8e2708155664472493e7c8a6a604d59
deva-p-stack <devap677@gmail.com>
2026-05-03 16:55:51 +0530

    fix: next config

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,34 +1,17 @@
-import path from "path";
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
   images: {
     unoptimized: true,
   },
-  turbopack: {
-    root: __dirname,
-  },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: "http://localhost:5000/:path*", // Your Node.js backend
-  //     },
-  //   ];
-  // },
 
   async rewrites() {
-  return [
-    {
-      source: "/api/:path*",
-      destination: "http://localhost:5000/:path*"
-    },
-  ];
-}
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
